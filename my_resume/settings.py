b1e8ceece8d57b086cbc48db9a50b63a0ac70daf
Amirul Islam <amirulislamalmamun@gmail.com>
2021-01-12 00:36:49 +0600

    fixed static

--- my_resume/settings.py
+++ my_resume/settings.py
@@ -121,9 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'resume_app/static/'),
     # os.path.join(BASE_DIR, 'resume_app/static/')
 ]
