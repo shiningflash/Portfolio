f9a31793264531b7d303a5a620a61ad72795539e
Amirul Islam <amirulislamalmamun@gmail.com>
2021-07-24 20:56:36 +0600

    debug false and some important update

--- my_resume/urls.py
+++ my_resume/urls.py
@@ -19,10 +19,16 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.conf.urls import url
+from django.views.static import serve 
+
 urlpatterns = [
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+
     path('admin/', admin.site.urls),
     path('', include('resume_app.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
